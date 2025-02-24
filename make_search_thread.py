96332483d9b29ddc6ca6174ab4e580fe8d260ee6
mhwewe <hrkhrg54@gmail.com>
2025-02-25 02:18:08 +0330

    Full responsiveness
    redesigned the entire UI to now be responsive
    every widget is now in a layout no more floating widgets
    instead of using images for the transparent backgrounds I now use CSS and added drop shadows
    added resizing to the frameless window(it is buggy)
    realized that the gotolink is not working correctly fixed it a bit but it is still broken for stuff like "volt_prime_set" being "volt"
    cahnged some of the icons, also I tried to make them animated when clicked and I failed(the static Icons are gifs)

--- make_search_thread.py
+++ make_search_thread.py
@@ -16,20 +16,6 @@ def make_search_thread(self, item, item_name, start_index):
     return worker, thread
 
 
-def make_search_thread_s(self, item, item_name, start_index):
-    thread = QtCore.QThread()
-    worker = SearchWorker(item.text())
-    worker.moveToThread(thread)
-
-    thread.started.connect(worker.run_2)
-    worker.finished.connect(thread.quit)
-    worker.finished.connect(worker.deleteLater)
-    thread.finished.connect(thread.deleteLater)
-    worker.result.connect(lambda result: self.search(result, start_index, item_name))
-    thread.start()
-    return worker, thread
-
-
 class SearchWorker(QtCore.QObject):
     finished = QtCore.pyqtSignal()
     result = QtCore.pyqtSignal(object)
@@ -48,11 +34,11 @@ class SearchWorker(QtCore.QObject):
         finally:
             self.finished.emit()
 
-    def run_2(self):
-        try:
-            result = main(self.search_text, arcane=True)  # initializes the item fetcher
-            self.result.emit(result)
-        except Exception:
-            self.result.emit(None)
-        finally:
-            self.finished.emit()
+    # def run_2(self):
+    #     try:
+    #         result = main(self.search_text, arcane=True)  # initializes the item fetcher
+    #         self.result.emit(result)
+    #     except Exception:
+    #         self.result.emit(None)
+    #     finally:
+    #         self.finished.emit()
